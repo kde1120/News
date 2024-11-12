8d61a32c7602f41f59a38c03e90f57fe18fc7342
kde <kde@jinhak.com>
2024-11-12 17:59:44 +0900

    fix : gitignore 재적용

--- news/server/dist/news/news.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { NewsService } from "./news.service";
-export declare class NewsController {
-    private readonly newsService;
-    constructor(newsService: NewsService);
-    getNews(query?: string): Promise<import("./news.service").News[]>;
-}
